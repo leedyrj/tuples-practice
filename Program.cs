7db4b1fdaa3da197133df711007e633df50d0419
Robert Leedy <leedyrj@gmail.com>
2018-08-22 14:32:00 -0500

    individual products work, need to get them all to add/multiply together

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transaction = new List<(string, double, int)>();
+
+            transaction.Add(("Computers", 1999.99, 3));
+            transaction.Add(("Phones", 999.99, 7));
+            transaction.Add(("Games", 59.99, 42));
+            transaction.Add(("DVD's", 24.99, 24));
+            transaction.Add(("Tablets", 575.99, 13));
+
+            foreach ((string product, double amount, int quantity) t in transaction)
+            {
+                // Logic goes here to look up quantity and amount in each transaction
+                Console.WriteLine($"{t.product} sold today: {t.quantity}");
+                Console.WriteLine($"Total revenue:{t.quantity * t.amount}");
+            }
+
+        }
+    }
+}
