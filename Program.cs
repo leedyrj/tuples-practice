dc178b073973fea99721f8544b0014afbe477bb9
Robert Leedy <leedyrj@gmail.com>
2018-08-22 14:49:42 -0500

    added steve's solution

--- Program.cs
+++ Program.cs
@@ -20,8 +20,31 @@ namespace tuples
                 // Logic goes here to look up quantity and amount in each transaction
                 Console.WriteLine($"{t.product} sold today: {t.quantity}");
                 Console.WriteLine($"Total revenue:{t.quantity * t.amount}");
+                Console.WriteLine();
             }
 
+            //steve's answer
+            (string product, double amount, int quantity) hammer = ("Hammer", 19.99, 25);
+            (string product, double amount, int quantity) nails = ("nails", 9.99, 25);
+            (string product, double amount, int quantity) screws = ("screws", 1.99, 25);
+            (string product, double amount, int quantity) staples = ("staples", 5.99, 25);
+            (string product, double amount, int quantity) level = ("level", 39.99, 25);
+
+            transaction.Add(hammer);
+            transaction.Add(nails);
+            transaction.Add(screws);
+            transaction.Add(staples);
+            transaction.Add(level);
+
+            int TotalProductsSold = 0;
+            double TotalRevenue = 0;
+            foreach ((string product, double amount, int quantity) t in transaction)
+            {
+                // Logic goes here to look up quantity and amount in each transaction
+                TotalProductsSold = +t.quantity;
+                TotalRevenue += t.quantity * t.amount;
+            }
+            Console.WriteLine($"We sold {TotalProductsSold} for a total revenue of {TotalRevenue}");
         }
     }
 }
